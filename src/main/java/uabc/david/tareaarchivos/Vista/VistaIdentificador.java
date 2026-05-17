90eeabfbe6e64e7d83e723701f4d997032be438e
David <mario.guerrero.marquez@uabc.edu.mx>
2026-05-16 21:12:17 -0700

    Agregué la lógica del botón que está en la vista del identificador de archivos y parece funcionar sin problemas.

--- src/main/java/uabc/david/tareaarchivos/Vista/VistaIdentificador.java
+++ src/main/java/uabc/david/tareaarchivos/Vista/VistaIdentificador.java
@@ -1,4 +1,6 @@
 package uabc.david.tareaarchivos.Vista;
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
 import uabc.david.tareaarchivos.IdentificadorArchivos;
 
 import javafx.geometry.Insets;
@@ -9,6 +11,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class VistaIdentificador {
     private IdentificadorArchivos identificador;
@@ -28,8 +34,31 @@ public class VistaIdentificador {
         identificar.setStyle("-fx-font-size: 14px; -fx-padding: 8px 15px;");
 
         Label nombreArchivo = new Label("Archivo: Sin seleccionar");
-        Label resultado = new Label("Resultado: ...");
+        Label resultado = new Label("Formato del archivo: ...");
         resultado.setStyle("-fx-font-size: 16px; -fx-text-fill: #1b5e20; -fx-font-weight: bold;");
+
+        identificar.setOnAction(e -> {
+            File archivo = new FileChooser().showOpenDialog(stage);
+            if (archivo != null) {
+                nombreArchivo.setText("Archivo: " + archivo.getName());
+                try {
+                    ArrayList<Integer> bytesLeidos = identificador.leer8Bytes(archivo);
+
+                    String formatoDetectado = identificador.verificarFormato(bytesLeidos);
+
+                    resultado.setText("Formato del archivo: " + formatoDetectado);
+
+                } catch (IOException ex) {
+                    Alert alerta = new Alert(Alert.AlertType.ERROR);
+                    alerta.setTitle("Error generado");
+                    alerta.setHeaderText(null);
+                    alerta.setContentText("No se pudieron leer los bytes: " + ex.getMessage());
+                    alerta.showAndWait();
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
 
         VBox contenedor = new VBox(15, instrucciones, identificar, nombreArchivo, resultado);
         contenedor.setAlignment(Pos.CENTER);
