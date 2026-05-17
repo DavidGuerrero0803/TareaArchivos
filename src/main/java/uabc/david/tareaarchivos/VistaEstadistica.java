02bac019765ee9eb38d086ed5c7fa04367a0b4b8
David <mario.guerrero.marquez@uabc.edu.mx>
2026-05-16 18:11:39 -0700

    Implementada la acción del botón que selecciona, carga y procesa el archivo de texto .txt

--- src/main/java/uabc/david/tareaarchivos/VistaEstadistica.java
+++ src/main/java/uabc/david/tareaarchivos/VistaEstadistica.java
@@ -5,13 +5,19 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class VistaEstadistica {
     private EstadisticaTexto analizador;
@@ -46,8 +52,29 @@ public class VistaEstadistica {
 
         tabla.getColumns().addAll(infoColumna, valorColumna);
 
-        selector.setOnAction(e -> {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Archivos de Texto (*.txt)", "*.txt")
+        );
 
+        selector.setOnAction(e -> {
+            File archivo = fileChooser.showOpenDialog(stage);
+            if (archivo != null) {
+                try {
+                    HashMap<String, Integer> estadisticas = analizador.realizarEstadistica(archivo);
+
+                    datosTabla.clear();
+                    datosTabla.add(new TablaElemento("Líneas", String.valueOf(estadisticas.get("Lineas"))));
+                    datosTabla.add(new TablaElemento("Palabras", String.valueOf(estadisticas.get("Palabras"))));
+                    datosTabla.add(new TablaElemento("Caracteres", String.valueOf(estadisticas.get("Caracteres"))));
+                } catch (Exception ex) {
+                    Alert alerta = new Alert(Alert.AlertType.ERROR);
+                    alerta.setTitle("Error generado");
+                    alerta.setHeaderText(null);
+                    alerta.setContentText("No pudo cargarse el archivo: " + ex.getMessage());
+                    alerta.showAndWait();
+                }
+            }
         });
 
         VBox contenedorBoton = new VBox(selector);
